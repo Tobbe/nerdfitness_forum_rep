c477be33aad2fbd6fc69af279eb518d076e37af4
Tobbe Lundberg <tobbe@tlundberg.com>
2019-12-24 15:01:43 +0100

    v0.2

--- NF_forum_rep.user.js
+++ NF_forum_rep.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NerdFitness forum rep
 // @namespace    https://github.com/tobbe
-// @version      0.1
+// @version      0.2
 // @description  NerdFitness forum - Display reputation
 // @author       Tobbe
 // @license      MIT
